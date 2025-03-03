dafe616f50f2deecc632c164805c9a72e6d6e5a1
lw384 <15651756926@163.com>
2025-03-03 18:20:02 +0800

    fix: update to use action

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const path = require('path')
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  // nitro:{
-  //   output:{
-  //     publicDir: path.join(__dirname,'docs')
-  //   }
-  // },
   app:{
     baseURL:"/resume/",
-    buildAssetsDir:'nuxt_assets',
   },
-  experimental:{
-    payloadExtraction:false,
-  }
 })
