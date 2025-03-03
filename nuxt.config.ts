80452bcbba81fa3f0609d22e72fc0188d9d0fd93
lw384 <15651756926@163.com>
2025-03-03 17:53:51 +0800

    feat: change nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,16 @@ const path = require('path')
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  nitro:{
-    output:{
-      publicDir: path.join(__dirname,'docs')
-    }
-  },
+  // nitro:{
+  //   output:{
+  //     publicDir: path.join(__dirname,'docs')
+  //   }
+  // },
   app:{
     baseURL:"/resume/",
+    buildAssetsDir:'nuxt_assets',
+  },
+  experimental:{
+    payloadExtraction:false,
   }
 })
