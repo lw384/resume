1b7b98882a85621d9133400b7c751a8b8a2588dd
lw384 <15651756926@163.com>
2025-03-03 17:06:25 +0800

    feat: change output path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const path = require('path')
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro:{
+    output:{
+      publicDir: path.join(__dirname,'docs')
+    }
+  }
 })
